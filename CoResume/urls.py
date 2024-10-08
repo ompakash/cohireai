236dc1ecd29644d7ef229d33432988590ea2d5e3
ompakash <opytc2@gmail.com>
2024-10-08 20:47:37 +0530

    change in structure

--- CoResume/urls.py
+++ CoResume/urls.py
@@ -1,6 +1,6 @@
 # pdfapp/urls.py
 from django.urls import path
-from .views import *
+from .api_views import *
 
 urlpatterns = [
     path('upload_resume/', ResumePDFUpload.as_view(), name='upload_resume'),
